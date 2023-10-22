4d1651ab36f951c267c924c86dad8e6f757f7eea
varshini39 <ragavs95@gmail.com>
2023-10-22 14:52:06 +0530

    User microservice for flight booking

--- src/main/java/com/app/user/UserServiceApplication.java
+++ src/main/java/com/app/user/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightbookingApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightbookingApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
